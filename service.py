8784fa9923b6f7d28e5e912204d83c9b0479ca8e
Michael Sherwood <maaknhb@gmail.com>
2024-11-04 22:25:22 +0200

    Evaluate Query Fields
    Resolves #1
    - Replaced hardcoded __get_sequences() with __get_entities()
    - Replaced __get_query_field_schema() with simplified logic in get_data()
    - Replaced __format_summary_query() with QueryField class
    - Removed unused __get_query_data() stub
    - Removed unused __apply_query_data() stub
    - Added QueryField class to handle formatting query field conditions into filters for summarize function and calling sg.summarize function

--- service.py
+++ service.py
@@ -1,11 +1,10 @@
+"""Service module for interacting with Shotgrid."""
+
 import os
 
 from shotgun_api3 import Shotgun
 from dotenv import load_dotenv
-
-# dev imports
-import json  # NOTE Remove for production
-import time  # NOTE Remove for production
+from typing import Any
 
 # Load credentials from .env file
 load_dotenv()
@@ -13,15 +12,11 @@ load_dotenv()
 
 class ShotgridQuery:
     def __init__(
-            self,
-            project_id: int = 85,
-            entity_type: str = "Sequence",
-            query_fields: list = ["sg_cut_duration", "sg_ip_versions"]
-        ) -> None:
-        self.project_id = project_id
+        self, entity_type: str = "", filters: list = [], fields: list = []
+    ) -> None:
         self.entity_type = entity_type
-        self.query_fields = query_fields
-
+        self.filters = filters
+        self.fields = fields
 
     def create_shotgrid_connection(self) -> Shotgun:
         """Create a connection to Shotgrid.
@@ -39,6 +34,73 @@ class ShotgridQuery:
 
         return sg
 
+    def get_data(self, entity_type: str, filters: list = [], fields: list = []) -> dict:
+        """Get entity data including query fields from Shotgrid.
+
+        Args:
+            entity_type (str): The name of the entity type to get data for
+            filters (list, optional): A list of filters to apply to the query. Defaults to [].
+            fields (list, optional): A list of fields to return in the query. Defaults to [].
+
+        Returns:
+            dict: A dictionary of data from Shotgrid
+        """
+        self.entity_type = entity_type
+        self.filters = filters
+        self.fields = fields
+
+        sg = self.create_shotgrid_connection()
+
+        # Get entities from SG
+        entities = self.__get_entities(sg, self.entity_type, self.filters, self.fields)
+
+        # Exit early if no entities are found
+        if not entities:
+            return []
+
+        # Get schema for entity type
+        schema = self.__get_schema(sg, self.entity_type)
+
+        # Filter out query fields that are not in self.fields
+        query_fields = {}
+
+        for field_name, field_metadata in schema.items():
+            if (
+                "query" in field_metadata.get("properties", {})
+                and field_name in self.fields
+            ):
+                query_fields[field_name] = schema[field_name]
+
+        # Populate query fields if they exist
+        if query_fields:
+            updated_entities = []
+
+            for entity in entities:
+                updated_entity = self.__populate_query_fields(sg, entity, query_fields)
+                updated_entities.append(updated_entity)
+
+            return updated_entities
+
+        sg.close()
+        return entities
+
+    def __get_entities(
+        self, sg: Shotgun, entity_type: str, filters: list = [], fields: list = []
+    ) -> list:
+        """Get all entities for a project.
+
+        Args:
+            sg (Shotgun): An instance of the Shotgrid API
+            entity_type (str): The name of the entity type to get entities for
+            filters (list, optional): A list of filters to apply to the query. Defaults to [].
+            fields (list, optional): A list of fields to return in the query. Defaults to [].
+
+        Returns:
+            list: A list of entities for the project
+        """
+        entities = sg.find(entity_type=entity_type, filters=filters, fields=fields)
+
+        return entities
 
     def __get_schema(self, sg: Shotgun, entity_type: str) -> dict:
         """Get the schema for an entity type.
@@ -55,71 +117,242 @@ class ShotgridQuery:
 
         return schema
 
-
-    def __get_query_field_schema(self, schema: dict, fields: list = []) -> list:
-        """Get the query fields from the schema for an entity type.
-
-        Args:
-            schema (dict): The entity type schema
-            fields (list, optional): A list of query fields to search for
-
-        Returns:
-            list: The query fields detected in a schema
-        """
-        # Define a list to store the query fields
-        query_fields = []
-
-        # Loop through the schema and get the query fields
-        for field_name, field_metadata in schema.items():
-
-            # Skip fields that are not in the fields list
-            # this is used to filter out built-in ShotGrid query fields
-            if fields and field_name not in fields:
-                continue
-
-            # Check if the field has a query property
-            if field_metadata.get("properties") and field_metadata["properties"].get("query"):
-
-                # Get the properties metadata
-                properties_metadata = field_metadata["properties"]
-
-                # Declare variables to store in the query fields list
-                _field_data_type = field_metadata.get("data_type", {}).get("value")
-                _entity_type = field_metadata.get("entity_type", {}).get("value")
-
-                _summary_default = properties_metadata.get("summary_default", {}).get("value")
-                _summary_field = properties_metadata.get("summary_field", {}).get("value")
-                _query = properties_metadata.get("query", {}).get("value")
-
-                # Create a dictionary to store the query field
-                _query_field = {
-                    "field": field_name,
-                    "field_data_type": _field_data_type,
-                    "entity_type": _entity_type,
-                    "summary_default": _summary_default,
-                    "summary_field": _summary_field,
-                    "query": _query,
-                }
-
-                # Append the query field to the query fields list
-                query_fields.append(_query_field)
-
-        return query_fields
-
-
-    def __get_sequences(self, sg: Shotgun, project_id: int) -> list:
-        """Get all sequences for a project.
+    def __populate_query_fields(
+        self, sg: Shotgun, entity: dict, query_fields: dict
+    ) -> dict:
+        """Populate query fields for an entity.
 
         Args:
             sg (Shotgun): An instance of the Shotgrid API
-            project_id (int): The ID of the project to get sequences for
+            entity (dict): The entity to populate query fields for
+            query_fields (dict): A dictionary of query fields to populate
 
         Returns:
-            list: A list of sequences for the project
+            dict: The entity with populated query fields
         """
-        filters = [["project", "is", {"type": "Project", "id": project_id}]]
-        fields = ["sg_cut_duration", "sg_ip_versions", "code"]
+        for field_name, field_metadata in query_fields.items():
+            query_field = QueryField(sg, field_name, field_metadata, entity)
+            query_field_data = query_field.get_query_data()
 
-        sequences = sg.find(entity_type="Sequence", filters=filters, fields=fields)
+            entity[field_name] = query_field_data
+        return entity
 
-        return sequences
+
+class QueryField:
+    def __init__(
+        self, sg: Shotgun, field_name: str, field_metadata: dict, entity: dict = None
+    ):
+        self.sg = sg
+        self.field_name = field_name
+        self.field_metadata = field_metadata
+        self.parent_entity = self.__get_parent_entity(entity)
+
+        self.properties = field_metadata.get("properties")
+
+        self.summary_default = self.properties.get("summary_default", {}).get("value")
+        self.summary_value = self.properties.get("summary_value", {}).get("value")
+        self.summary_field = self.properties.get("summary_field", {}).get("value")
+
+        self.query = self.properties.get("query", {}).get("value")
+
+        self.filters = self.query.get("filters")
+        self.entity_type = self.query.get("entity_type")
+
+    def get_query_data(self) -> Any:
+        """Retrieve query data based on the summary default value.
+
+        Depending on the value of `self.summary_default`, this method will:
+        - Perform a query calculation for "average", "count", "maximum", "minimum", or "sum".
+        - Perform a record count query for "record_count".
+        - Perform a single record query for "single_record".
+        - Raise a ValueError for any other value.
+
+        Returns:
+            Any: The result of the query based on the summary default value.
+
+        Raises:
+            ValueError: If the summary default value is not supported.
+        """
+        if self.summary_default in [
+            "average",
+            "count",
+            "maximum",
+            "minimum",
+            "sum",
+        ]:
+            return self.query_calculation(calculation_type=self.summary_default)
+        elif self.summary_default == "record_count":
+            return self.query_record_count()
+        elif self.summary_default == "single_record":
+            return self.query_single_record()
+        else:
+            raise ValueError(f"Summary Default {self.summary_default} not supported.")
+
+    def get_sg_filters(self) -> list:
+        """Convert query field conditions to Shotgrid filters.
+
+        Returns:
+            list: A list of Shotgrid filters
+        """
+        conditions = self.filters
+        filters = self.__convert_conditions_to_filter(conditions)
+
+        return [filters]
+
+    def query_calculation(self, calculation_type: str) -> Any:
+        """Perform a query calculation on the Shotgrid entity.
+
+        Args:
+            calculation_type (str): The type of calculation to perform. Supported values are:
+                - "average"
+                - "count"
+                - "maximum"
+                - "minimum"
+                - "sum"
+
+        Returns:
+            Any: The result of the query calculation.
+        """
+        filters = self.get_sg_filters()
+        field = self.summary_field
+        summary_fields = [{"field": field, "type": calculation_type}]
+
+        summary = self.sg.summarize(
+            entity_type=self.entity_type, filters=filters, summary_fields=summary_fields
+        )
+
+        return summary["summaries"][field]
+
+    def query_record_count(self) -> int:
+        """Perform a record count query on the Shotgrid entity.
+
+        Returns:
+            int: The number of records returned by the query.
+        """
+        filters = self.get_sg_filters()
+        field = self.summary_field
+        summary_fields = [{"field": field, "type": "count"}]
+
+        summary = self.sg.summarize(
+            entity_type=self.entity_type, filters=filters, summary_fields=summary_fields
+        )
+
+        return summary["summaries"][field]
+
+    def query_single_record(self) -> str:
+        """Perform a single record query on the Shotgrid entity.
+
+        Returns:
+            str: The result of the query.
+        """
+        filters = self.get_sg_filters()
+        field = self.summary_field
+
+        sg_results = self.sg.find(
+            entity_type=self.entity_type, filters=filters, fields=[field]
+        )
+
+        if not sg_results:
+            return None
+
+        results = []
+        for entity in sg_results:
+            if isinstance(entity.get(field), dict):
+                results.append(entity[field].get("name"))
+            else:
+                results.append(entity[field])
+
+        return ", ".join(results)
+
+    def logical_operator_lookup(self, operator: str) -> str:
+        """Lookup table for logical operators.
+
+        Args:
+            operator (str): The logical operator to lookup.
+
+        Returns:
+            str: The Shotgrid logical operator.
+        """
+        operator_lookup = {"and": "all", "or": "any"}
+        if operator in operator_lookup.keys():
+            return operator_lookup[operator]
+        raise ValueError(f"Logical Operator {operator} not found in lookup.")
+
+    def __convert_conditions_to_filter(self, conditions: list) -> list:
+        """Convert conditions to Shotgrid filters.
+
+        Args:
+            conditions (list): A list of conditions to convert to filters.
+
+        Returns:
+            list: A list of Shotgrid filters.
+        """
+        sg_filters = []
+
+        logical_operator = self.logical_operator_lookup(
+            conditions.get("logical_operator")
+        )
+
+        for condition in conditions.get("conditions", []):
+            if "conditions" in condition.keys():
+                # Recursively convert conditions subgroup to filter
+                nested_filters = self.__convert_conditions_to_filter(condition)
+
+                # Append the nested filter group
+                sg_filters.append(nested_filters)
+            else:
+                # Process condition
+                active = condition.get("active", True)
+                path = condition.get("path")
+                relation = condition.get("relation")
+                values = condition.get("values", [])
+
+                if not active:
+                    continue
+
+                # Check if value needs to be replaced with parent_entity
+                lookup_values = []
+                for value in values:
+                    if (
+                        isinstance(value, dict)
+                        and value.get("valid") == "parent_entity_token"
+                    ):
+                        lookup_values.append(self.parent_entity)
+                    else:
+                        lookup_values.append(value)
+
+                # Use single value if only one value is present
+                value = lookup_values
+                if len(lookup_values) == 1:
+                    value = lookup_values[0]
+
+                sg_filters.append([path, relation, value])
+
+        return {"filter_operator": logical_operator, "filters": sg_filters}
+
+    def __get_parent_entity(self, entity: dict) -> dict:
+        """Get the parent entity from the entity dictionary.
+
+        Args:
+            entity (dict): The entity to get the parent entity from.
+
+        Returns:
+            dict: The parent entity dictionary.
+        """
+        keys = ["type", "id"]
+        parent_entity = {}
+
+        for key in keys:
+            parent_entity[key] = entity.get(key, None)
+
+        return parent_entity
+
+
+if __name__ == "__main__":
+    filters = [["project", "is", {"type": "Project", "id": 85}]]
+    fields = ["code", "sg_cut_duration", "sg_ip_versions"]
+
+    sg_query = ShotgridQuery()
+    sequences = sg_query.get_data(
+        entity_type="Sequence", filters=filters, fields=fields
+    )
